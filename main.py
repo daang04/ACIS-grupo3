408c45221799b083182b203e4904c713e48629e2
Nestor20193767 <74310266+Nestor20193767@users.noreply.github.com>
2024-10-16 00:32:59 -0500

    Update main.py
    NOMBRE

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main():
     # URL en formato RAW del icono
     logo_url = "https://raw.githubusercontent.com/daang04/ACIS-grupo3/main/icon_MEDGAN%20(1).png"
     
-    st.set_page_config(page_icon = logo_url)
+    st.set_page_config(page_icon = logo_url, page_title='MEDGAN')
     # favicon being an object of the same kind as the one you should provide st.image() with (ie. a PIL array for example) 
     #or a string (url or local file path)
     
